4986a2e749cb6741d5e1bdfc6ab8e6576d4064c4
mengawwubetu <mengaww2ya@gmail.com>
2023-10-14 10:46:12 -0700

    mengah

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            System.out.println("hello sema...");
+            // for you, but you can always add more by pressing Ctrl+
+        }
+    }
+}
